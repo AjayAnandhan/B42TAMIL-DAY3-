ff60f702bd35b5858e28019bfe7d2da3503ebfd6
AjayAnandhan <117960858+AjayAnandhan@users.noreply.github.com>
2023-01-30 18:45:56 +0530

    Task

--- Task Q1.js
+++ Task Q1.js
@@ -5,22 +5,29 @@ var aj = {
     "colors" : ["black", "red", "green"]
 }
 
-// // FOR LOOP:
+// FOR LOOP:
 
 for (var i = 0; i < aj.colors.length; i++) {
     console.log(aj.name, "at the age of", aj.age[i], "liked",aj.colors[i],"color");
 }
 console.log("----------------------------------------------");
 
-// // FOR IN LOOP:
+// FOR IN LOOP:
 for(var b in aj){
     console.log(b,":", aj[b])
 }
 console.log("----------------------------------------------");
 
-// //FOR OF LOOP:
+//FOR OF LOOP:
 
-for(let obj of aj){
-    console.log(obj.colors)
+var a = ["Pirates","of","the","carriebian"]
+let text = "";
+for (let x of a) {
+  text += x;
 }
+console.log(text)
 console.log("----------------------------------------------");
+
+// FOR EACH:
+
+var b = a.forEach((arr)=>console.log(arr));
