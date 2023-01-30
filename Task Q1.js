a0e535782c1b1b89c2a91353a53b976d26db4629
AjayAnandhan <117960858+AjayAnandhan@users.noreply.github.com>
2023-01-30 18:55:45 +0530

    Task

--- Task Q1.js
+++ Task Q1.js
@@ -1,3 +1,4 @@
+// Q1
 var aj = {
     "name" : "Ajay",
     "age" : ["30","111","550"],
@@ -31,3 +32,24 @@ console.log("----------------------------------------------");
 // FOR EACH:
 
 var b = a.forEach((arr)=>console.log(arr));
+
+
+console.log("-----------------------------------------_____Q2_____----------------------------------------------------");
+ // Q2
+ 
+ 
+var a = {
+    "Name": "Ajay",
+    "Age" : "22",
+    "DOB" : "02/02/2000",
+    "Email" : "a@email.com",
+    "Phone" : "555",
+    "Qualification" : ["10th - 75%","12th - 76%","Degree - 7.5%"],
+    "LanguagesKnown" : ["Tamil", "English"],
+    "Skills" : ["JavaScript" ,"HTML","CSS","Python"]
+}
+for(var b in a){
+    console.log(b,":" +a[b]);
+}
+
+// console.log(a)
